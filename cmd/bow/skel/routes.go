fb6cb77245c4a2d107632a97b652d0c327d09648
Loric Brevet <loric.brevet@gmail.com>
2022-10-18 14:09:49 +0200

    Update routes for assets to work with httprouter

--- cmd/bow/skel/routes.go
+++ cmd/bow/skel/routes.go
@@ -7,12 +7,13 @@ import (
 )
 
 func (app *application) routes() http.Handler {
-	// chain applied to all dynamic routes
+	// middleware chain applied to all dynamic routes
 	dynamic := app.DynChain()
 
 	router := httprouter.New()
-	router.Handler(http.MethodGet, "/assets/", app.FileServer())
+	router.Handler(http.MethodGet, "/assets/*filepath", app.FileServer())
 
+	// application routes
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 
 	return app.StdChain().Then(router)
